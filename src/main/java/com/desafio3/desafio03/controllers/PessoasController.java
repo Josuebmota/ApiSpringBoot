29e8a44eb59c96e0447c9cfd0a51949ce1848934
Josuebmota <josuebatistam1@gmail.com>
2020-03-12 21:57:18 -0300

    Finalizando Rotas

--- src/main/java/com/desafio3/desafio03/controllers/PessoasController.java
+++ src/main/java/com/desafio3/desafio03/controllers/PessoasController.java
@@ -1,10 +1,52 @@
 package com.desafio3.desafio03.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.http.*;
+
+import com.desafio3.desafio03.models.Pessoas;
+import com.desafio3.desafio03.respository.PessoasRepository;
+
+import lombok.RequiredArgsConstructor;
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/pessoas")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class PessoasController {
 
+  @Autowired
+  private PessoasRepository pessoasRepository;
+
+  @PostMapping("/adicionar")
+  public ResponseEntity<Pessoas> create(@Valid @RequestBody Pessoas product) {
+    return new ResponseEntity<Pessoas>(pessoasRepository.save(product), new HttpHeaders(), HttpStatus.CREATED);
+  }
+
+  @PutMapping(value = "/{id}/atualizar")
+  public ResponseEntity<?> update(@PathVariable("id") long id, @Valid @RequestBody Pessoas pessoa) {
+    return pessoasRepository.findById(id).map(p -> {
+      p.setFirstName(pessoa.getFirstName());
+      p.setLastName(pessoa.getLastName());
+      p.setCareer(pessoa.getCareer());
+      Pessoas updated = pessoasRepository.save(p);
+      return ResponseEntity.ok().body(updated);
+    }).orElse(ResponseEntity.notFound().build());
+  }
+
+  @DeleteMapping(path = { "/{id}/deletar" })
+  public ResponseEntity<?> delete(@PathVariable long id) {
+    return pessoasRepository.findById(id).map(p -> {
+      pessoasRepository.deleteById(id);
+      return ResponseEntity.ok().build();
+    }).orElse(ResponseEntity.notFound().build());
+  }
+
+  @GetMapping("/listar")
+  public ResponseEntity<List<Pessoas>> findAll() {
+    return new ResponseEntity<List<Pessoas>>(pessoasRepository.findAll(), new HttpHeaders(), HttpStatus.OK);
+  }
 }
--- src/main/java/com/desafio3/desafio03/models/Pessoas.java
+++ src/main/java/com/desafio3/desafio03/models/Pessoas.java
@@ -1,7 +1,11 @@
 package com.desafio3.desafio03.models;
 
+import lombok.*;
 import javax.persistence.*;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 @Table(name = "PESSOAS")
 public class Pessoas {
